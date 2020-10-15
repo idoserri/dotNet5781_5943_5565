c649b9bfe599050f3f730b95a35f71348185423d
Aaron Westreich <aaronwestreich2001@gmail.com>
2020-10-15 16:57:41 +0300

    implementation of function Welcome5565

---
+++ dotNet_5943_5565/dotNet_00_5943_5565/Program5565.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet_00_5943_5565
+{
+    partial class Program
+    {
+        static partial void Welcome5565()
+        {
+            Console.WriteLine("I am also here.");
+        }
+    }
+}
