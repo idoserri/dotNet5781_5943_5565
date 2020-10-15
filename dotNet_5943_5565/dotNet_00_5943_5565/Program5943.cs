d1baeccb928fba6b14b50b3b48e3aabbfec67fd4
ido serri <serri@g.jct.ac.il>
2020-10-15 16:44:35 +0300

    initial code + Welcome5565

---
+++ dotNet_5943_5565/dotNet_00_5943_5565/Program5943.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet_00_5943_5565
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome5943();
+            Welcome5565();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome5565();
+        private static void Welcome5943()
+        {
+            Console.WriteLine(" Enter your name: ");
+            string x = Console.ReadLine();
+            Console.WriteLine(x + ", welcome to my first console application");
+        }
+    }
+}
