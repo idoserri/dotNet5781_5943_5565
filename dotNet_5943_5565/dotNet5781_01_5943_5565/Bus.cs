a19ee5e82130e64bf9a2df19916d04e5a9adbfd5
ido serri <serri@g.jct.ac.il>
2020-10-22 11:37:17 +0300

    start of 1th exercise

---
+++ dotNet_5943_5565/dotNet5781_01_5943_5565/Bus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_5943_5565
+{
+    public class Bus
+    {
+        private int licenseNumber;
+        private DateTime startDate;
+        private int mileage;
+        private int fuelKM;
+        Bus(int license_Number, DateTime start_Date, int random_Mileage, int starting_Fuel_KM) 
+        {
+            licenseNumber = license_Number;
+            startDate = start_Date;
+            mileage = random_Mileage;
+            fuelKM = starting_Fuel_KM;
+        }
+
+        public DateTime StartDate { get => startDate; }
+        
+        public int LicenseNumber
+        {
+            get { return licenseNumber; }
+            private set
+            { 
+                if(value>99999999 || value< 1000000)
+                    Console.WriteLine("ERROR: license number is not valid");
+                else
+                    licenseNumber = value; 
+            }
+        }
+    }
+}
