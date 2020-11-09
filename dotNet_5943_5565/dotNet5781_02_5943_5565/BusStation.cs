f92133e351e5e3a9b86b3706dea92fc7ce849697
ido serri <serri@g.jct.ac.il>
2020-11-09 22:40:15 +0200

    some work

--- dotNet_5943_5565/dotNet5781_02_5943_5565/BusStation.cs
+++ dotNet_5943_5565/dotNet5781_02_5943_5565/BusStation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -58,7 +59,13 @@ namespace dotNet5781_02_5943_5565
        public Location Location
         {
             get => location;
-            set { location.Latitude = value.Latitude; location.Longitude = value.Longitude; }
+            set
+            {
+                if (value.Latitude > 90 || value.Latitude < -90 ||
+                    value.Longitude > 180 || value.Longitude < -180)
+                    throw new Exception("location is out of range");
+                location.Latitude = value.Latitude; location.Longitude = value.Longitude;
+            }
         }
 
         public string Address
@@ -72,10 +79,76 @@ namespace dotNet5781_02_5943_5565
             code = _code;
             address = _address;
             Random r = new Random();
-            double latit = r.Next(-90, 91);
-            double longt = r.Next(-180, 181);
+            double latit =  r.NextDouble() * (33.3 - 31) + 31;
+            double longt = r.NextDouble() * (35.5 - 34.3) + 34.3; 
             location.Latitude = latit;
             location.Longitude = longt;
+        }
+        public override string ToString()
+        {
+            return "Bus Station Code: " + code + ", " +
+                location.Latitude + "°N " + location.Longitude + "°E\n";
+        }
+
+    }
+
+    class BusStationLine : BusStation
+    {
+        private double distanceFromLastStationKM;
+        private TimeSpan timeFromLastStation;
+        public BusStationLine(double distance, TimeSpan time, int _code, string _address)
+            : base(_code, _address)
+        {
+
+            distanceFromLastStationKM = distance;
+            timeFromLastStation = time;
+        }
+
+        public double DistanceFromLastStationKM
+        {
+            get => distanceFromLastStationKM;
+            set => distanceFromLastStationKM = value;
+        }
+
+        public TimeSpan TimeFromLastStation
+        {
+            get => timeFromLastStation;
+            set => timeFromLastStation = value;
+        }
+    }
+
+    enum Areas { General, North, South, Center, Jerusalem }
+    class BusLine
+    {
+        private List<BusStationLine> stations;
+        private int line;
+        private BusStationLine firstStation;
+        private BusStationLine lastStation;
+        private Areas area;
+
+        public BusLine(List<BusStationLine> course, int number, BusStationLine FStation, BusStationLine LStation,Areas a)
+        {
+            area = a;
+            stations = course;
+            line = number;
+            firstStation = FStation;
+            lastStation = LStation;
+
+            if (stations.Last<BusStationLine>() != LStation)
+                stations.Add(LStation);
+            if (stations.First<BusStationLine>() != FStation)
+            {
+                stations.Reverse();
+                stations.Add(FStation);
+                stations.Reverse();
+            }
+        }
+        public override string ToString()
+        {
+            string str = "line: " + line.ToString() + "\n" + "area: " + area + "\n" + "stations: ";
+            foreach (BusStationLine item in stations)
+                str += item.Code.ToString()+ " ";
+            return str + "\n";
         }
     }
 }
